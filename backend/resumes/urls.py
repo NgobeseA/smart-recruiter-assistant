0b8190a90522b5a052c218d470fd8d8841e83e37
ngobesea <andilekngobese@gmail.com>
2025-07-14 15:48:07 +0200

    updates on the admin jobs and resumes urls

--- backend/resumes/urls.py
+++ backend/resumes/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('upload/', views.UploadResumeView.as_view(), name='upload_resume'),
-    path('list/', views.ResumeListView.as_view(), name='list_resumes'),
+    # path('list/', views.ResumeListView.as_view(), name='list_resumes'),
+    path('semantic-list/<int:job_id>/', views.ResumeListWithScoreView.as_view(), name='resume_list_with_score'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
