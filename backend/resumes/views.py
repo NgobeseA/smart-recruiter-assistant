516dd9fa6e8daf85d75bcc856627f6c2a19e2179
ngobesea <andilekngobese@gmail.com>
2025-07-16 15:34:40 +0200

    evaluating how much a cv scored based on the required skills

--- backend/resumes/views.py
+++ backend/resumes/views.py
@@ -7,7 +7,7 @@ import pdfplumber
 from .models import Resume
 from .serializers import ResumeSerializer
 from jobs.models import JobPost
-from scoring.scoring_engine import semantic_score
+from scoring.scoring_engine import semantic_score, evaluate_resume
 
 # Create your views here.
 class UploadResumeView(APIView):
@@ -38,15 +38,17 @@ class ResumeListWithScoreView(APIView):
         except JobPost.DoesNotExist:
             return Response({'error': 'Job not found'}, status=status.HTTP_404_NOT_FOUND)
         
-        resumes = Resume.object.filter(job)
-        resume_data = []
+        resumes = Resume.object.filter(job=job)
+        results = []
+
+        required_skills = job.required_skills.split(',') if job.required_skills else []
 
         for resume in resumes:
-            score = semantic_score(resume.parsed_text, job.description)
-            print(f"score from {resume} resume: {score}")
-            serialized = ResumeSerializer(resume).data
-            print(f"serialized data: {serialized}")
-            serialized['score'] = score
-            resume_data.append(serialized)
-        
-        return Response(resume_data, status=status.HTTP_200_OK)
+            feedback = evaluate_resume(resume.parsed_text, job.description,required_skills)
+            data = ResumeSerializer(resume).data
+            data["semantic_score"] = feedback["semantic_score"]
+            data["matched_skills"] = feedback["matched_skills"]
+            data["missing_skills"] = feedback["missing_skills"]
+            results.append(data)
+            
+        return Response(results, status=status.HTTP_200_OK)
